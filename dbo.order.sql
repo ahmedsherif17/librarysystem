ad82607b6f63017454d3e419dce68083f4b11eec
Ahmed Sherif <81445449+ahmedsherif17@users.noreply.github.com>
2022-09-08 04:47:22 +0200

    Add files via upload

---
+++ dbo.order.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[order] (
+    [time]   TEXT          NULL,
+    [id]     INT           NOT NULL,
+    [pizza]  NVARCHAR (50) NULL,
+    [drink]  NVARCHAR (50) NULL,
+    [pieceZ] INT           NULL,
+    [pieceD] INT           NULL,
+    [place]  NVARCHAR (50) NULL,
+    [Ntable] INT           NULL,
+    CONSTRAINT [PK_order_1] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
